e073d8f44b9171dd3c3d196dd5de944a770174e1
Nick Egorov <keooqnick@gmail.com>
2025-07-24 09:06:08 +0300

    feat: add ask_menu_number and check_valid_number

--- logic/menu_functions.py
+++ logic/menu_functions.py
@@ -1,10 +1,32 @@
 import time as t
 
+def show_local_time():
+    print(t.strftime("%d %B %Y, %H:%M"))
+    print()
+
 def show_menu(menu: dict):
     """Shows any menu to user"""
     for number, action in menu.items():
         print(f"{number}) {action}")
 
-def show_local_time():
-    print(t.strftime("%d %B %Y, %H:%M"))
-    print()
+def check_valid_number():
+    """Check if the number is not a str"""
+    while True:
+        try:
+            number = int(input("\nWrite the number here: "))
+        except ValueError:
+            print("---------------------------")
+            print("This is not a number. Try again, please.")
+        else:
+            return number
+
+def ask_menu_number(menu: dict):
+    """Ask a menu number and check does it exist"""
+    while True:
+        menu_number = check_valid_number()
+        if menu_number not in menu.keys():
+            print("---------------------------")
+            print("There is no action with that number. Try again, "
+                "please.")
+        else:
+            return menu_number
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from data import menu_data as md
 from logic import menu_functions as mf
 
-print("-- To-do App --")
-mf.show_local_time()
-mf.show_menu(md.main_menu)
+while True:
+    print("-- To-do App --")
+    mf.show_local_time()
+    mf.show_menu(md.main_menu)
+    mf.ask_menu_number(md.main_menu)
