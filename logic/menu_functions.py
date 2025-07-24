d5c5a1355fed3a64b001ec3b0ef6eda8e514814b
Nick Egorov <keooqnick@gmail.com>
2025-07-24 08:55:11 +0300

    feat: add show_local_time

--- logic/menu_functions.py
+++ logic/menu_functions.py
@@ -1,4 +1,10 @@
+import time as t
+
 def show_menu(menu: dict):
     """Shows any menu to user"""
     for number, action in menu.items():
         print(f"{number}) {action}")
+
+def show_local_time():
+    print(t.strftime("%d %B %Y, %H:%M"))
+    print()
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from data import menu_data as md
 from logic import menu_functions as mf
 
 print("-- To-do App --")
+mf.show_local_time()
 mf.show_menu(md.main_menu)
