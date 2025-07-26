558052aa419d672c9648e6f8595ffde932137957
Nick Egorov <keooqnick@gmail.com>
2025-07-26 16:44:48 +0300

    refactor: rename file menu_data to data_structures

--- logic/to_do_functions.py
+++ logic/to_do_functions.py
@@ -1,10 +1,10 @@
-from data import menu_data as md
+from data import data_structures as ds
 
 def show_to_do_list():
     """Show to-do list to user"""
     print("-- My To-Do List --")
-    if md.tasks:
-        for number, task in md.tasks.items():
+    if ds.tasks:
+        for number, task in ds.tasks.items():
             print(f"{number}) {task}")
     else:
         print("There is no tasks yet.")
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from data import menu_data as md, file_functions as ff
+from data import data_structures as ds, file_functions as ff
 from logic import menu_functions as mf
 
 while True:
     print("-- To-do App --")
     mf.show_local_time()
-    mf.show_menu(md.main_menu)
-    menu_number = mf.ask_menu_number(md.main_menu)
+    mf.show_menu(ds.main_menu)
+    menu_number = mf.ask_menu_number(ds.main_menu)
     print()
     if not menu_number: # If user chooses «exit»
         break
